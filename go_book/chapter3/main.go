2510e9836ad1d1ef64ed1fb5c8da9ffea5edb1f1
Phil Opaola <phillipsopaolacoding@gmail.com>
2013-09-11 22:01:27 -0400

    Completed chapter 3 of An Introduction to Programming in Go

---
+++ go_book/chapter3/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("1.0 + 1.0 = ", 1.0+1.0)
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World")
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println("32132 x 42452:", 32132*42452)
+}
